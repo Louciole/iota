740969536ba4229f463e121a547e8ddf9af3b563
Lou ! <loha@odoo.com>
2023-11-24 16:17:54 +0100

    🥉bad drag and drop interface

---
+++ sources/main.js
@@ -0,0 +1,22 @@
+snippets={
+    "btn":"<div class='btn'>Magifique bouton</div>"
+}
+
+function drag(e) {
+    e.dataTransfer.setData("snippet", e.target.classList[1]);
+    var img = document.createElement("img");
+    img.src = "http://kryogenix.org/images/hackergotchi-simpler.png";
+    e.dataTransfer.setDragImage(img, 0, 0);
+}
+
+function allowDrop(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+    e.preventDefault();
+    const snippet = e.dataTransfer.getData("snippet");
+    const node= document.createElement('div')
+    node.innerHTML=snippets[snippet]
+    e.target.appendChild(node);
+}
